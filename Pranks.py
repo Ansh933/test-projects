d9a9cb17f4d55bee31b3f44e0635306d7d54b7e0
Ansh933 <82750605+Ansh933@users.noreply.github.com>
2021-05-26 14:09:05 +0530

    You can prank your friends!
    Make sure to not show code to them!

---
+++ Pranks.py
@@ -0,0 +1,19 @@
+password = input("Enter your password ")
+if password == "covid is the worst":
+    print("welcome!")
+    input("open hak.exe? ")
+    print("hak.exe is open!")
+    input("tranfer and back up? ")
+    print("CRITIAL ERROR: CLOUD DESRTOYED")
+    print("emergerncy police all activated!")
+    conts = input("continue? ")
+    if conts == "yes":
+        print("calling")
+    else:
+        print("k stopping")
+    print ("lol")
+    print("lol")
+    print("virus has deleted drive: C")
+    print("Happy (month) fools")
+else:
+    print("wrong pass restart and enter password as covid is the worst")
